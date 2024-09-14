80cd551fd62effce88d86b2ff1b09454b41c9583
Luke <lukesmb@gmail.com>
2024-09-14 13:27:08 +0200

    Init

---
+++ rccar/cmdhandler.ino
@@ -0,0 +1,70 @@
+
+static esp_err_t cmd_handler(httpd_req_t *req){
+  char*  buf;
+  size_t buf_len;
+  char variable[32] = {0,};
+  
+  buf_len = httpd_req_get_url_query_len(req) + 1;
+  if (buf_len > 1) {
+    buf = (char*)malloc(buf_len);
+    if(!buf){
+      httpd_resp_send_500(req);
+      return ESP_FAIL;
+    }
+    if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+      if (httpd_query_key_value(buf, "go", variable, sizeof(variable)) == ESP_OK) {
+      } else {
+        free(buf);
+        httpd_resp_send_404(req);
+        return ESP_FAIL;
+      }
+    } else {
+      free(buf);
+      httpd_resp_send_404(req);
+      return ESP_FAIL;
+    }
+    free(buf);
+  } else {
+    httpd_resp_send_404(req);
+    return ESP_FAIL;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  //flip the camera vertically
+  s->set_vflip(s, 1);          // 0 = disable , 1 = enable
+  // mirror effect
+  s->set_hmirror(s, 1);          // 0 = disable , 1 = enable
+
+  int res = 0;
+  
+  if(!strcmp(variable, "up")) {
+    Serial.print("Going Forward");
+    digitalWrite(RELAY, LOW);
+    delay(1);
+    digitalWrite(RELAY, HIGH);
+  }else if(!strcmp(variable, "left")) {
+    Serial.print("Turning Left");
+    digitalWrite(RELAY, LOW);
+    delay(1);
+    digitalWrite(RELAY, HIGH);
+  }else if(!strcmp(variable, "right")) {
+    Serial.print("Turning Right");
+    digitalWrite(RELAY, LOW);
+    delay(1);
+    digitalWrite(RELAY, HIGH);
+  }else if(!strcmp(variable, "down")) {
+    Serial.print("Reversing");
+    digitalWrite(RELAY, LOW);
+    delay(1);
+    digitalWrite(RELAY, HIGH);
+  } else {
+    res = -1;
+  }
+
+  if(res){
+    return httpd_resp_send_500(req);
+  }
+
+  httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+  return httpd_resp_send(req, NULL, 0);
+}
